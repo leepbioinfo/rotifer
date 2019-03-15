df390a4890d12f4a59c1a35ecb5df03e16ccfca1
Robson Francisco de Souza <rfsouza@usp.br>
2019-03-15 16:06:08 -0300

    Bye, bye __init__.py at rotifer's root...

--- __init__.py
+++
@@ -1,8 +0,0 @@
-# # # Import modules
-# #
-# from rotifer.seq import seq as rseq
-# # # # from rotifer import rseq
-# from rotifer.tools.search import search as rsearch
-# # # from rotifer import rsearch => search class
-# # #
-# from rotifer.tools.search import domain as rdomain
