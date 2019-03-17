d7c53dea59937012b3a1bf12b2f2be3dc7f7fb08
Robson Francisco de Souza <rfsouza@usp.br>
2019-03-16 22:32:32 -0300

    Fixing library path discovery for consensus.py

--- bin/consensus.py
+++ bin/consensus.py
@@ -3,10 +3,8 @@
 # For now, we use this as part of our script's template
 import os
 import sys
-_add_path = [ os.path.join(os.path.dirname(os.path.dirname(__file__)), "lib"),
-        os.path.join(os.path.dirname(os.path.dirname(__file__)), "lib", "python" + str(sys.version_info.major) + "." + str(sys.version_info.minor), "site-packages")
-        ]
-for _d in _add_path:
+_d = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+for _d in [ os.path.join(_d, "lib"), os.path.join(_d, "lib", "python" + str(sys.version_info.major) + "." + str(sys.version_info.minor), "site-packages") ]:
     if os.path.exists(_d):
         sys.path.insert(0,_d)
 import rotifer.core.cli as corecli
