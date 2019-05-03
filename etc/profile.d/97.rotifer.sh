214ded34e5ab80833504cf308b8a7b40d9c2f91d
Robson Francisco de Souza <rfsouza@usp.br>
2019-05-03 17:54:48 -0300

    Dropping temporary variable ROTIFER_PATH from bash's environment

--- etc/profile.d/97.rotifer.sh
+++ etc/profile.d/97.rotifer.sh
@@ -1,3 +1,4 @@
 ROTIFER_PATH=$(echo $(readlink -f ${BASH_SOURCE[0]}) | sed 's/rotifer\/.*/rotifer/')
 append_directory_to_path $ROTIFER_PATH/bin
 add_to_environment_variable PYTHONPATH first $ROTIFER_PATH/lib
+unset ROTIFER_PATH
