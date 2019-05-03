0df26df23abf83289ddb224429ff70781c785fe0
Robson Francisco de Souza <rfsouza@usp.br>
2019-05-03 18:08:07 -0300

    Moving all Rotifer related bash initialization code to etc/profile.d

--- etc/profile.d/90.rotifer.functions.sh
+++ etc/profile.d/90.rotifer.functions.sh
@@ -21,6 +21,26 @@ function dom_count() {
     architecture2table $arch | tgroup -f '$F[2]=$F[1];1' -g 0 -g 1 -a 2=count -i 0..2 "$@"
 }
 
+function acc2pfam() {
+    (
+        efetch -db protein -format fasta -id $1 | hmmscan --cpu 4 /databases/pfam/Pfam - | hmmer2table -s -c;
+        efetch -db protein -format fasta -id $1 | phobius 2> /dev/null | phobius2table -e 0.0101
+    ) \
+    | domain2architecture -e 0.0101 \
+    | architecture2table \
+    | padtable
+}
+
+function acc2profiledb() {
+    (
+        efetch -db protein -format fasta -id $1 | rpsblast -db ~/data/rpsdb/allprofiles | blast2table -c profiledb -s;
+        efetch -db protein -format fasta -id $1 | phobius 2> /dev/null | phobius2table -e 0.0101
+    ) \
+    | domain2architecture -e 0.0101 \
+    | architecture2table \
+    | padtable
+}
+
 # List table columns
 function tdesc() {
     sep=$2;
