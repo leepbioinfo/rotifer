e41c82c3b08a4839f9ee1bd2ba8e4f83da1a5849
Robson Francisco de Souza <rfsouza@usp.br>
2019-10-02 18:12:38 -0300

    Adding BASH function aln_order_by_tree

--- etc/profile.d/90.rotifer.functions.sh
+++ etc/profile.d/90.rotifer.functions.sh
@@ -51,3 +51,12 @@ function tdesc() {
     head -n1 $1 | ttranspose -s $sep | tfilter -f 'unshift(@F,$H{c}++);1'
 }
 
+# Sort alignment by tree
+function aln_order_by_tree() {
+	aln=$1
+	tree=$2
+	tmp=$(mktemp -p /tmp aln_order_by_tree.XXXXXX)
+	treeutil -of leaves $tree > $tmp
+	aln2seqrows -r '\t' $aln | tjoin -r '\n' -i1 1 -i2 1 -1 1 -f1 '$F[1] ne "ID"' -f '$F[0]=">$F[0]";1' -p $tmp -
+	rm -f $tmp
+}
