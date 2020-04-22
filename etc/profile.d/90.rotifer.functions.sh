af30cece5a95754a29f091ffd745edf47d32a550
Robson Francisco de Souza <rfsouza@usp.br>
2020-04-22 12:40:40 -0300

    Fixing path for acc2profiledb function.

--- etc/profile.d/90.rotifer.functions.sh
+++ etc/profile.d/90.rotifer.functions.sh
@@ -34,7 +34,7 @@ function acc2pfam() {
 
 function acc2profiledb() {
     (
-        efetch -db protein -format fasta -id $1 | rpsblast -db ~/data/rpsdb/allprofiles | blast2table -c profiledb -s;
+        efetch -db protein -format fasta -id $1 | rpsblast -db $ROTIFER_DATA/rpsdb/allprofiles | blast2table -c profiledb -s;
         efetch -db protein -format fasta -id $1 | phobius 2> /dev/null | phobius2table -e 0.0101
     ) \
     | domain2architecture -e 0.0101 \
