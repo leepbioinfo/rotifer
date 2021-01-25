e96dc18f8f9a6801f69faa49d846717b2c414d92
Robson Francisco de Souza <rfsouza@usp.br>
2021-01-25 11:51:37 -0300

    Remove script: will move to doc

--- etc/profile.d/97.rotifer.sh
+++
@@ -1,6 +0,0 @@
-ROTIFER_PATH=$(echo $(readlink -f ${BASH_SOURCE[0]}) | sed 's/rotifer\/.*/rotifer/')
-append_directory_to_path $ROTIFER_PATH/bin
-add_to_environment_variable PYTHONPATH first $ROTIFER_PATH/lib
-add_to_environment_variable PERL5LIB last $ROTIFER_PATH/perl/lib
-#export ROTIFER_DATA=/databases
-unset ROTIFER_PATH
