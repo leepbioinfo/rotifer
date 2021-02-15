50ed30f28d0d532e8b93b8557d90aab0f8fd1b9b
Robson Francisco de Souza <rfsouza@usp.br>
2021-02-15 02:10:26 -0300

    new rgaparser: genome to table or FASTA

--- lib/rotifer/genome/io.py
+++ lib/rotifer/genome/io.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
 # Core libraries
-import types
 import itertools
+import os
+import re
+import sys
+import types
 
 # Pandas
 import numpy as np
@@ -10,13 +13,35 @@ import pandas as pd
 
 # Biopython
 from Bio import SeqIO
-#from Bio.Alphabet import generic_dna
 
-# Rotifer libraries
-from rotifer.core.functions import not_kwargs
-from rotifer.taxonomy.utils import lineage as rtlineage
+def _get_basename(path):
+    # Parse filename
+    bn = path
+    if path == sys.stdin:
+        bn = "__stdin__"
+    elif os.path.exists(path):
+        bn = os.path.splitext(os.path.basename(path))[0]
+    else:
+        raise TypeError("Unknown file-like object")
+    return bn
 
-def parse(handle, informat, *args, **kwargs):
+def SequenceNameToAssembly(path, s):
+    return s.name
+
+def GenbankAssemblyFromFilename(path, s):
+    """
+    Parse filenames with the regular expression
+    
+    ^(GC[AF]_\d+\.\d+)_\S+\.gbff(\.gz)?
+    
+    to extract Genbank assembly IDs.
+    """
+    aid = _get_basename(path)
+    if re.match(r'^(GC[AF]_\d+\.\d+)',aid):
+        aid = re.sub('^(GC[AF]_\d+\.\d+).*','\\1',aid)
+    return aid
+
+def parse(handle, informat, *args, assembly=GenbankAssemblyFromFilename, **kwargs):
     """
     Load genomic data from sequence file(s).
     This method crates an iterator capable of return SeqRecords.
@@ -24,22 +49,69 @@ def parse(handle, informat, *args, **kwargs):
     Arguments:
      - handle   : one or more file handles or filenames
      - informat : either gff or a Bio.SeqIO supported format
+
+     - assembly : rule for setting genome assembly accession ID
+                  Assembly identifiers are not part of the INSDC 
+                  (http://www.insdc.org/) standard but can be
+                  found in some NCBI entries under the DBLINK
+                  tag, such as in many RefSeq entries:
+
+                  DBLINK    BioProject: PRJNA224116
+                            BioSample: SAMEA3138382
+                            Assembly: GCF_000210855.2 <-- here!
+
+                  This parameter allows the user to automate the definition
+                  of genome identifiers. You can use either a string or a
+                  lambda function that takes two arguments: file path and
+                  sequence record object.
+
+                  See GenbankAssemblyFromFilename and SequenceNameToAssembly
+                  for examples of function compatible with this parameter.
+
      - retvalue : object(s) type to return. Supported objects are
 
      Other arguments depend on the parser (see methods below)
     """
     if not isinstance(handle,list):
         handle = [handle]
-    for eachhandle in handle:
-        if informat == "gff":
-            for s in gff(eachhandle, *args, **kwargs):
-                yield s
-        else:
-            #for s in SeqIO.parse(eachhandle,informat,alphabet=generic_dna):
-            for s in SeqIO.parse(eachhandle,informat):
-                yield s
 
-def gff(path, seqid=None, assembly=None, organism=None, strain=None, description=None, topology='linear', keywords=[], ete3=None):
+    # Process each file, return and wait for next request
+    for eachhandle in handle:
+        # Choose parser
+        if informat == "gff":
+            generator = gff(eachhandle, *args, **kwargs)
+        else:
+            generator = SeqIO.parse(eachhandle,informat)
+
+        # Parse file basename
+        bn = _get_basename(eachhandle)
+
+        # Process each Bio.SeqRecord
+        for s in generator:
+            # Change number-only sequence names
+            if re.match('^\d+$',s.id):
+                s.id = bn + ":" + s.id
+                s.name = s.id
+
+            # Fix DESCRIPTION
+            if not s.description:
+                s.description = s.name + ", whole genome shotgun sequence"
+
+            # Fix assembly
+            if not any([ "Assembly:" == x[0:9] for x in s.dbxrefs ]):
+                if assembly:
+                    if isinstance(assembly,types.FunctionType):
+                        assemblyStr = str(assembly(eachhandle,s))
+                    else:
+                        assemblyStr = assembly
+                    s.dbxrefs.append(f'Assembly:{assemblyStr}')
+                else:
+                    s.dbxrefs.append(f'Assembly:{s.name}')
+
+            # Return and wait next call
+            yield s
+
+def gff(path, seqid=None, organism=None, strain=None, description=None, topology='linear', keywords=[], ete3=None):
     """
     Parse GFF file
 
@@ -50,12 +122,10 @@ def gff(path, seqid=None, assembly=None, organism=None, strain=None, description
                       - the sequence object (a Bio.SeqRecord.SeqRecord object)
                       - the input file path
      - description : set sequence description
-                     Could be a lambda function to apply to the file path
-     - assembly    : genome assembly accession
-                     Could be a lambda function to apply to the file path
+                     Could be a lambda function with two arguments: file path and Bio.SeqRecord
      - organism    : organism name or NCBI taxid
      - strain      : add this string to the organism name but not to its taxonomy
-                     Could be a lambda function to apply to the file path
+                     Could be a lambda function with two arguments: file path and Bio.SeqRecord
      - topology    : sequence topology, circular or linear
      - keywords    : keywords to add to annotation
      - ete3        : ete3 object to use when fetching taxonomy
@@ -82,7 +152,7 @@ def gff(path, seqid=None, assembly=None, organism=None, strain=None, description
     l = h.readlines()
     h.close()
     n = [ x for x in range(0,len(l)) if l[x] == "##FASTA\n" ][0]
-    sio = SeqIO.parse(StringIO("".join(l[n+1:])),"fasta", alphabet=generic_dna)
+    sio = SeqIO.parse(StringIO("".join(l[n+1:])),"fasta")
     seqrecords = OrderedDict({ x.id: x for x in sio })
     with TempFile() as t:
         # Dump tabular section of GFF to temporary file
@@ -104,22 +174,12 @@ def gff(path, seqid=None, assembly=None, organism=None, strain=None, description
                 s.annotations["organism"] = organism.loc[0,'organism']
                 if strain:
                     if isinstance(strain,types.FunctionType):
-                        strainStr = strain(path)
+                        strainStr = strain(path,s)
                     else:
                         strainStr = strain
                     s.annotations["organism"] = s.annotations["organism"] + strainStr
                 s.annotations["source"]   = s.annotations["organism"]
                 s.annotations["taxonomy"] = organism.loc[0,'taxonomy'].split("; ")
-
-            # Fix assembly name
-            if assembly:
-                if isinstance(assembly,types.FunctionType):
-                    assemblyStr = assembly(path)
-                else:
-                    assemblyStr = assembly
-                s.dbxrefs.append(f'Assembly:{assemblyStr}')
-            else:
-                s.dbxrefs.append(f'Assembly:{s.name}')
 
             # Add or set keywords
             if keywords:
@@ -131,9 +191,9 @@ def gff(path, seqid=None, assembly=None, organism=None, strain=None, description
             # Set description
             if description:
                 if isinstance(description,types.FunctionType):
-                    descriptionStr = description(path)
+                    descriptionStr = description(path,s)
                 else:
-                    descriptionStr = assembly
+                    descriptionStr = description
                 s.description = descriptionStr
 
             # Set topology
