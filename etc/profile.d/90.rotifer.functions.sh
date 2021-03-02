aa1b9189e06838081dd06b79a428613b9cf41c23
Robson Francisco de Souza <rfsouza@usp.br>
2021-03-02 16:53:00 -0300

    Fixing hmmer2table's clean option in acc2pfam

--- etc/profile.d/90.rotifer.functions.sh
+++ etc/profile.d/90.rotifer.functions.sh
@@ -24,7 +24,7 @@ function dom_count() {
 function acc2pfam() {
     SEQ=$(efetch -db protein -format fasta -id $1)
     (
-        echo "$SEQ" | hmmscan --cpu 4 /databases/pfam/Pfam - | hmmer2table -c;
+        echo "$SEQ" | hmmscan --cpu 4 /databases/pfam/Pfam - | hmmer2table -c version;
         echo "$SEQ" | phobius 2> /dev/null | phobius2table -e 0.0101
     ) \
     | domain2architecture -e 0.0101 \
