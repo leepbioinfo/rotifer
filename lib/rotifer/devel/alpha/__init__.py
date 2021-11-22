9ae6614a4e06a5a1cc403c21a03bc59202129462
Gianlucca Gonçalves Nicastro <ggnicastro@gmail.com>
2021-11-22 18:26:00 -0300

    update  init file

--- lib/rotifer/devel/alpha/__init__.py
+++ lib/rotifer/devel/alpha/__init__.py
@@ -1,3 +1,6 @@
-import *
-def hello():
-  print("Hello uga!")
+# To import p.py file  as a module :
+#from . import p
+
+# To import the hello() function  into the alpha module :
+from p  import hello 
+
