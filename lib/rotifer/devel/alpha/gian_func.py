c3a2c0a66e94f7526eb41edc3375b9ab49f210db
Gianlucca Gonçalves Nicastro <ggnicastro@gmail.com>
2021-12-10 12:28:04 -0300

    Create gian_func.py

---
+++ lib/rotifer/devel/alpha/gian_func.py
@@ -0,0 +1,39 @@
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
+def count_series(series, normalize=False, cut_off=False, count='domain'):
+    ''' 
+    Function to flatten a pd.Series.value_counts and count the number of domains recognized in the whole proteins set of proteins
+    Mind that by defaul, it count domain not architecture. If one wants to count architecture, it should change the count option to 'architecture'.
+    The normalize options is to write the results as frequency.
+    The cut_off options is to print results only above a given threashold. If the normalize function is True, you should give the cutoff value as pct. 
+    Usage:
+    in: count_series(df.pfam, normalize=True, cut_off=10)
+    out:'T6SS_HCP(15.15%), SP(11.36%)'
+    in: df.groupby('c80e3').agg({'pfam':count_series})
+                                                 pfam
+    c80e3                                            
+    AAN66278.1                                       
+    ABO22067.1                           SP(1), TM(1)
+    ABU79335.1  Gal_mutarotas_2(1), Glyco_hydro_31(1)
+    ABV15410.1                           SP(1), TM(1)
+    ABV15414.1                                  TM(1)
+
+    '''
+    l = []
+    if count == 'architecture':
+        s = series.dropna().value_counts(normalize=normalize)
+    else:
+        s = series.str.split('+').explode().dropna().value_counts(normalize=normalize)
+
+    if cut_off:
+        cut_off = cut_off/100
+        s = s.where(lambda x: x>= cut_off).dropna()
+    for y,z in s.iteritems():
+        if normalize:
+            l.append(f'{y}({100 * z:.2f}%)')
+        else:
+            l.append(f'{y}({z})')
+    return ', '.join(l)
