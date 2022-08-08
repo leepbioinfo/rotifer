ff827df90d12d0401b115569a89c407370331676
rodolfoar <rodolfo.alvarenga.ribeiro@usp.br>
2022-08-08 19:06:48 -0300

    updated rodolfo in alpha

--- lib/rotifer/devel/alpha/rodolfo.py
+++ lib/rotifer/devel/alpha/rodolfo.py
@@ -1,10 +1,32 @@
 #!/usr/bin/env python3
 
 
+#todo
+
+def annotate_community(df, column='community', reference='query', ):
+    '''
+    add or modify communities in a dataframe
+    Parameters:
+        df: Pandas dataframe
+        dataframe to be annotated
+        column:
+        rows: which rows to annotate
+        reference: 
+    '''
+    # loc to change info
+    # or map
+    # df.loc[rows, column] = df[reference].map(dict)
+    # dict is a result of a df2comm
+    # cut communities by cluster size and inputs two tables
+    # annotated table and graph
+    return
+
+
 def dataframe_to_community(df, source='query', target='hit', weight='probability', directed=False):
     '''
     Extract communities from a dataframe using the Louvain algorithm.
     '''
+    import pandas as pd
     import numpy as np
     import networkx as nx
     import community
@@ -19,7 +41,7 @@ def dataframe_to_community(df, source='query', target='hit', weight='probability
         )
         df = df.sort_values(weight).drop_duplicates([source, target])
 
-    G = nx.from_pandas_edgelist(df[[source, target, weight]], edge_attr=weight)
+    G = nx.from_pandas_edgelist(df.filter([source, target, weight]), source=source, target=target, edge_attr=weight)
     partition = community.best_partition(G, weight=weight)
     d = (
         pd.DataFrame.from_dict(partition, orient="index")
