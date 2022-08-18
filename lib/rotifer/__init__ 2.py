18dd64b7c1055f627c9dd0efdb14efb907e54c5f
Gianlucca Gonçalves Nicastro <ggnicastro@gmail.com>
2022-08-18 10:42:07 -0400

    add to_png function to sequence class

---
+++ lib/rotifer/__init__ 2.py
@@ -0,0 +1,14 @@
+__doc__ = '''
+Rapid Open-source Tools and Infrastructure For data Exploration and Research
+============================================================================
+
+Rotifer is a multi-language collection of high-level programming libraries
+for the development of data analysis pipelines, mostly targeting problems in
+comparative genomics and the computational analysis of biological sequences.
+
+It also provides a collection of easy to use command line tools based on this framework.
+'''
+
+# Import modules
+from rotifer.core import GlobalConfig
+from rotifer.core.functions import loadConfig
