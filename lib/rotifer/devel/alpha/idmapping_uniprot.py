c3fa3ba81fa27694025fbddd4fabc5711d569fb5
rodolfoar <rodolfo.alvarenga.ribeiro@usp.br>
2023-02-09 19:46:51 -0300

    function genbank_to_uniprot retrieves possible accessions and if available returns a alphafold pdb url

--- lib/rotifer/devel/alpha/idmapping_uniprot.py
+++ lib/rotifer/devel/alpha/idmapping_uniprot.py
@@ -182,6 +182,19 @@ def get_data_frame_from_tsv_results(tsv_results):
 # Test Run with few acc:
 # Usage
 
+def genbank_to_uniprot(from_db="EMBL-GenBank-DDBJ_CDS", to_db="UniProtKB", ids=["BAE76179.1","AAC73502.1"]):
+    job_id = submit_id_mapping(
+        from_db=from_db,
+        to_db=to_db,
+        ids=ids)
+    if check_id_mapping_results_ready(job_id):
+        link = get_id_mapping_results_link(job_id)
+        results = get_id_mapping_results_stream(link+"?compressed=true&fields=accession%2Cxref_alphafolddb%2C&format=tsv")
+    r = get_data_frame_from_tsv_results(results)
+    r['urlAF'] = [ "https://alphafold.ebi.ac.uk/files/AF-" + x + "-F1-model_v4.pdb" for x in r.loc[r.AlphaFoldDB.str.split(';', expand=True)[0] == r.Entry].Entry]
+    return(r)
+
+
 '''
 job_id = submit_id_mapping(
     from_db="EMBL-GenBank-DDBJ_CDS",
@@ -202,6 +215,7 @@ job_id = submit_id_mapping(
 if check_id_mapping_results_ready(job_id):
     link = get_id_mapping_results_link(job_id)
     results = get_id_mapping_results_stream(link+"?compressed=true&fields=accession%2Cxref_alphafolddb%2C&format=tsv")
-get_data_frame_from_tsv_results(results)
+r = get_data_frame_from_tsv_results(results)
+r['urlAF'] = [ "https://alphafold.ebi.ac.uk/files/AF-" + x + "-F1-model_v4.pdb" for x in r.loc[r.AlphaFoldDB.str.split(';', expand=True)[0] == r.Entry].Entry]
 
 '''
