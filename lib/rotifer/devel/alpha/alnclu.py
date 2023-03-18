6a8e89cd10b288864c35cddcc00bf36a2962d6d4
Rodolfo A RIbeiro <rodolfo.alvarenga.ribeiro@usp.br>
2023-03-18 15:37:33 -0300

    Update alnclu.py

--- lib/rotifer/devel/alpha/alnclu.py
+++ lib/rotifer/devel/alpha/alnclu.py
@@ -6,16 +6,16 @@
 import os
 import pandas as pd
 
-def alnclu(info, c80e3="c80e3", i1c1="i1c1", index, i=3):
+def alnclu(info, c80e3="c80e3", c100i100="c80i70", i=3):
     '''
     Function to generate alignments for all clusters indicated by c80e3
     takes a indexed multifasta by esl-sfetch as index, and aggregates the
-    clusters by a higher level cluster, in this case, i1c1.
+    clusters by a higher level cluster, in this case, c100i100.
     parameter i is 3 by default, increasing it exclude small clusters
     index.fa file should be at current directory,
     or it's relative path defined as well as the index.ssi.
     Usage:
-    alnclu(pd.Dataframe, 'c8e3', 'i1c1', 'complete.faa')
+    alnclu(pd.Dataframe, 'c80e3', 'c100i100', 'complete.faa')
     '''
 
     # Validate input
@@ -26,11 +26,10 @@ def alnclu(info, c80e3="c80e3", i1c1="i1c1", index, i=3):
     curr_dir = os.getcwd()
     os.makedirs(f'{curr_dir}/clusters/hhmdb/aln')
     for x in info.c80e3.unique():
-        if info[info.c80e3 == x].i1c1.nunique() >= i:
+        if info[info.c80e3 == x].c100i100.nunique() >= i:
             os.mkdir(f'{curr_dir}/clusters/{x}')
             os.chdir(f'{curr_dir}/clusters/{x}')
-            info[info.c80e3 == x].i1c1.drop_duplicates().to_csv(f'{x}.{c80e3}.acc', sep="\t", index=None, header=None)
-            os.system(f'esl-sfetch -f {curr_dir}/{index} {x}.{c80e3}.acc | mafft --thread 16 --localpair --maxiterate 1000  > {x}.{c80e3}.aln')
+            info[info.c80e3 == x].c100i100.drop_duplicates().to_list(), local_database_path={curr_dir}/{index}).align(method='linsi').to_file({x}.{c80e3}.aln)
         os.chdir(f'{curr_dir}')
 
     os.chdir('clusters')
@@ -41,4 +40,4 @@ def alnclu(info, c80e3="c80e3", i1c1="i1c1", index, i=3):
 
     os.chdir('aln')
     os.system(f'for x in *.aln; do hhsearch -i $x -d ../{index} -M 50;done')
-    os.system('python3 /home/leep/ggnicastro/bin/hhsearch_table.py')
+    #os.system('python3 /home/leep/ggnicastro/bin/hhsearch_table.py')
