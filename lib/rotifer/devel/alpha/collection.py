45d8511675d371027eedba48effe19976f411c8f
Robson Francisco de Souza <rfsouza@usp.br>
2023-06-02 11:03:39 -0300

    A few changes to collection.py

--- lib/rotifer/devel/alpha/collection.py
+++ lib/rotifer/devel/alpha/collection.py
@@ -4,29 +4,69 @@ import os
 import pandas as pd
 from glob import glob
 from rotifer.devel.beta import sequence as rdbs
+config = {
+        "informat": {
+            "*.sto": "stockholm",
+            "*.fa": "fasta",
+            "*.fasta": "fasta",
+        }
+}
 
 # Loading our alignments
 class SequenceCollection():
     def __init__(
             self,
-            basedir = "/projects/salmonella/work/st",
-            suffix = "sto",
-            informat = "stockholm",
+            basedir = "/projects/salmonella/work/st/alndb",
+            pattern = "*.sto",
+            other_files = {
+                "pdb": {
+                    "basedir": "/projects/salmonella/work/st/colabfold",
+                    "pattern": "/*_rank_001_*.pdb",
+                }
+            }
         ):
-        alnpaths = glob(f'{basedir}/alndb/*.{suffix}')
+        if isinstance(pattern,str):
+            pattern = [pattern]
+
+        # Find alignments
         alndb = []
         alignments = dict()
-        for x in alnpaths:
-            basename = os.path.basename(x).replace(f".{suffix}","")
-            pdb = glob(os.path.join(basedir,"colabfold") + "/" + basename + "/*_rank_001_*.pdb")
-            if len(pdb):
-                pdb = pdb[0]
+        for extension in pattern:
+            if "/**/" in extension:
+                recursive = True
             else:
-                pdb=None
-            alndb.append((basename, x, pdb))
-            alignments[basename] = rdbs.sequence(x,informat)
-        alndb = pd.DataFrame(alndb, columns=['name','path','pdb'])
+                recursive = False
+
+            # Load data
+            for x in glob(f'{basedir}/{extension}', recursive=recursive):
+                # Cleanup target name
+                if extension[0] == "*":
+                    suffix = extension[1:]
+                else:
+                    suffix = extension
+                basename = os.path.basename(x).replace(f"{suffix}","")
+                alignments[basename] = rdbs.sequence(x,config["informat"][extension])
+
+                # Load other paths
+                otherfiles = []
+                othernames = []
+                for colname, other in other_files.items():
+                    files = glob(os.path.join(other["basedir"], basename + other["pattern"]))
+                    if len(files) == 0:
+                        files=None
+                    elif len(files) == 1:
+                        files = files[0]
+                    otherfiles.append(files)
+                    othernames.append(colname)
+
+                # Add data to stack
+                alndb.append((basename, x, files))
+
+        # Create internal dataframe for paths
+        alndb = pd.DataFrame(alndb, columns=['name','path',*othernames])
         alndb.index = alndb.name.tolist()
+
+        # Store data in object
         self.df = alndb
         self.alignment = alignments
 
@@ -41,18 +81,27 @@ class SequenceCollection():
         return self.df.index.tolist()
 
     # Saving to HTML
-    def tohtml(self, model, filename, sample=50, consensus=90):
+    def tohtml(self, model, filename=None, sample=50, consensus=90):
         aln = self[model].copy()
-        pdb = self.df.loc[model,"pdb"]
 
         keep = []
-        if pdb:
+        if "pdb" in self.df.columns:
+            pdb = self.df.loc[model,"pdb"]
+            if isinstance(pdb,list):
+                pdb = pdb[0]
             aln = aln.add_pdb(aln.df.iloc[0,0], pdb_file=pdb)
             keep = aln.df.iloc[0:2,0].tolist()
+
+        # Filter redundancy
+        aln.add_cluster(coverage=0, identity=70, inplace=True)
+        aln = aln.filter('c0i70 == id')
 
         if len(aln) > sample:
             keep = keep + aln.df[2:].id.sample(sample-2).tolist()
             aln = aln.filter(keep=keep) 
+
+        if filename == None:
+            filename = f'{model}.html'
 
         aln.to_html(consensus, filename, annotations=aln.df.iloc[0,0], remove_gaps=aln.df.iloc[1,0], fixed_index=True)
 
