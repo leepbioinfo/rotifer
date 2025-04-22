3a6014e71e4e75b95756c6062a570f0bf68e12d9
Thiago Roberto dos Santos <195175316+trdsantos@users.noreply.github.com>
2025-04-22 08:30:33 -0300

    feat(alpha): add compute_shannon_entropy function to trsantos.py

--- lib/rotifer/devel/alpha/trsantos.py
+++ lib/rotifer/devel/alpha/trsantos.py
@@ -1,22 +1,37 @@
 def summarize_taxonomic_levels(df, column, level):
-'''
-Howdy there!!
+    """
+    Summarizes the taxonomic composition of a dataset at a specified taxonomic level.
 
-This function aim is to create a taxonomic summary table given a dataframe (normally a ndf)
-df asks for the dataframe, column askes for the column with taxonomic info and level asks for the taxonomic level (1, 2, 3...)
+    Parameters
+    ----------
+    df : pd.DataFrame
+        The input DataFrame, typically a normalized data format (ndf) containing taxonomic lineages.
+    column : str
+        The name of the column in the DataFrame that contains taxonomic lineage strings.
+    level : int
+        The taxonomic level to summarize (1 for kingdom/domain, 2 for phylum, etc.).
+        Taxonomic levels are assumed to be separated by '>'.
 
-I do recommend using epsoares update_lineage function before using this one
+    Returns
+    -------
+    pd.DataFrame
+        A summary DataFrame with the count and percentage of entries at the specified taxonomic level.
+        Example output:
 
-Example usage:
+                    Count  Percentage
+        bacteria     11350   93.16
+        archaea        658    5.40
+        unclassified   175    1.44
 
-rad_sam_tax = summarize_taxonomic_levels(rad_sam.ndf, 'lineage', 1)
-rad_sam_tax.head() will output something like:
+    Notes
+    -----
+    It is recommended to run `epsoares.update_lineage` on your dataset before using this function.
 
-          Count  Percentage
-bacteria  11350   93.162604
-archaea     658    5.400969
-            175    1.436428
-'''
+    Example
+    -------
+    >>> summary = summarize_taxonomic_levels(rad_sam.ndf, 'lineage', 1)
+    >>> summary.head()
+    """
     import pandas as pd
 
     def extract_taxonomic_level(lineage, level):
@@ -42,3 +57,55 @@ archaea     658    5.400969
     })
 
     return summary
+
+def compute_shannon_entropy(self, ignore_gaps=True):
+    """
+    Computes Shannon entropy for each column in the alignment.
+
+    Parameters
+    ----------
+    ignore_gaps : bool
+        Whether to exclude gaps ('-' or '.') from entropy calculation.
+
+    Notes
+    -----
+    You need to run the following commands in order for this method to work properly:
+
+    from rotifer.devel.beta import sequence as rdbs
+    rdbs.sequence.compute_shannon_entropy = compute_shannon_entropy
+
+    Returns
+    -------
+    entropy : pd.Series
+        A pandas Series with entropy values indexed by column number.
+    """
+
+    from collections import Counter
+    import numpy as np
+    import pandas as pd
+
+
+    # Make sure we only work with sequences
+    sequences = self.df[self.df['type'] == 'sequence']['sequence'].tolist()
+
+    if not sequences:
+        raise ValueError("No sequences found to compute entropy.")
+
+    # Transpose the alignment into columns
+    alignment_length = len(sequences[0])
+    columns = zip(*sequences)  # Converts list of sequences to columns
+
+    entropy_values = []
+    for col in columns:
+        if ignore_gaps:
+            col = [res for res in col if res not in ['-', '.']]
+        freqs = Counter(col)
+        total = sum(freqs.values())
+        if total == 0:
+            entropy = 0
+        else:
+            probs = [count / total for count in freqs.values()]
+            entropy = -sum(p * np.log2(p) for p in probs if p > 0)
+        entropy_values.append(entropy)
+
+    return pd.Series(entropy_values, name='shannon_entropy')
