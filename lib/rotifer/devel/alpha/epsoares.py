60e32fc9457805d3088355951e024d2ddd7ac91a
Eduardo Pereira Soares <eduardo_soares@usp.br>
2025-10-10 13:03:30 -0300

    new pyhmmer functions

--- lib/rotifer/devel/alpha/epsoares.py
+++ lib/rotifer/devel/alpha/epsoares.py
@@ -333,13 +333,14 @@ def hmmscan(sequences, file=None, pfam_database_path='/databases/pfam/Pfam-A.hmm
     abc = ph.easel.Alphabet.amino()
     if file:
        seqs = ph.easel.SequenceFile(file, digital = True, alphabet=abc)
+    
     else:
        if type(sequences) == list:
-           seqobj = rdbs.sequence(sequences)
-   	   seqs = digitalize_seqobj(seqobj)
+       		seqobj = rdbs.sequence(sequences)
+       		seqs = digitalize_seqobj(seqobj)
 
        elif type(sequences) == rotifer.devel.beta.sequence.sequence:
-           seqs = digitalize_seqobj(sequences)
+       		seqs = digitalize_seqobj(sequences)
 
     #Hmmscan run and file processment
     h = list(ph.hmmer.hmmscan(seqs, hmms, cpus=cpus))
