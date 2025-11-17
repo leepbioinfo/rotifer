0bd16e4bc4f862ec81a5244c6c0cb99a903a0b56
Eduardo Pereira Soares <eduardo_soares@usp.br>
2025-11-17 17:08:13 -0300

    epsoares hmmscan update

--- lib/rotifer/devel/alpha/epsoares.py
+++ lib/rotifer/devel/alpha/epsoares.py
@@ -301,7 +301,7 @@ def make_hmm(seqobj, hmm_name='alignment', save='alignment.hmm'):
         
     return hmm
 
-def hmmscan(sequences, file=None, pfam_database_path='/databases/pfam/Pfam-A.hmm', cpus=0, columns=['aln_target_name', 'aln_hmm_name','i_evalue','c_evalue','score','env_score','aln_target_from','aln_target_to', 'aln_target_length', 'aln_hmm_length', 'env_from', 'env_to'], rename=True):
+def hmmscan(sequences, file=None, models_file=['/databases/pfam/Pfam-A.hmm'], cpus=0, columns=['aln_target_name', 'aln_hmm_name','i_evalue','c_evalue','score','env_score','aln_target_from','aln_target_to', 'aln_target_length', 'aln_hmm_length', 'env_from', 'env_to', 'source'], rename=True):
     
     '''
     Perform an hmmscan of protein sequences against a Pfam HMM database.
@@ -326,108 +326,110 @@ def hmmscan(sequences, file=None, pfam_database_path='/databases/pfam/Pfam-A.hmm
     #Progress bar callback
     def callback(hmm, hits):
         pbar.update(1)
-        
+    
+    results = []
     #HMM load
-    with ph.plan7.HMMFile(pfam_database_path) as hmm_file:
-       if hmm_file.is_pressed:
-           hmms = list(hmm_file.optimized_profiles())
-       else:
-           hmms = list(hmm_file)
-	
-    #Sequences load
-    abc = ph.easel.Alphabet.amino()
-    if file:
-       seqs = ph.easel.SequenceFile(file, digital = True, alphabet=abc)
-    
-    else:
-       if type(sequences) == list:
-       		seqobj = rdbs.sequence(sequences)
-       		seqs = digitalize_seqobj(seqobj)
+    for model in models_file:
+        with ph.plan7.HMMFile(model) as hmm_file:
+           if hmm_file.is_pressed:
+               hmms = list(hmm_file.optimized_profiles())
+           else:
+               hmms = list(hmm_file)
+	    
+        #Sequences load
+        abc = ph.easel.Alphabet.amino()
+        if file:
+           seqs = ph.easel.SequenceFile(file, digital = True, alphabet=abc)
+        
+        else:
+           if type(sequences) == list:
+           		seqobj = rdbs.sequence(sequences)
+           		seqs = digitalize_seqobj(seqobj)
 
-       elif type(sequences) == rotifer.devel.beta.sequence.sequence:
-       		seqs = digitalize_seqobj(sequences)
-    
-    pbar = tqdm(total=len(seqs), desc='hmmscan')
-    
-    #Hmmscan run and file processment
-    h = list(ph.hmmer.hmmscan(seqs, hmms, cpus=cpus, callback=callback))
-    r = []
-    for th in h:
-        target_name = th.query.name.decode() if th.query.name else None
-        found = False
-        for x in th:
-            for y in x.domains:
-                r.append({
-                    # pyhmmer.plan7.Domain attributes
-                    "hit":                   y.hit,
-                    "bias":                  y.bias,
-                    "c_evalue":              y.c_evalue,
-                    "correction":            y.correction,
-                    "env_from":              y.env_from,
-                    "env_to":                y.env_to,
-                    "env_score":             y.envelope_score,
-                    "i_evalue":              y.i_evalue,
-                    "pvalue":                y.pvalue,
-                    "score":                 y.score,
+           elif type(sequences) == rotifer.devel.beta.sequence.sequence:
+           		seqs = digitalize_seqobj(sequences)
+        
+        pbar = tqdm(total=len(seqs), desc='hmmscan')
+        
+        #Hmmscan run and file processment
+        h = list(ph.hmmer.hmmscan(seqs, hmms, cpus=cpus, callback=callback))
+        r = []
+        for th in h:
+            target_name = th.query.name.decode() if th.query.name else None
+            found = False
+            for x in th:
+                for y in x.domains:
+                    r.append({
+                        # pyhmmer.plan7.Domain attributes
+                        "hit":                   y.hit,
+                        "bias":                  y.bias,
+                        "c_evalue":              y.c_evalue,
+                        "correction":            y.correction,
+                        "env_from":              y.env_from,
+                        "env_to":                y.env_to,
+                        "env_score":             y.envelope_score,
+                        "i_evalue":              y.i_evalue,
+                        "pvalue":                y.pvalue,
+                        "score":                 y.score,
 
-                    # pyhmmer.plan7.Alignment attributes
-                    "aln_domain":            y.alignment.domain,
-                    "aln_hmm_accession":     y.alignment.hmm_accession.decode(),
-                    "aln_hmm_from":          y.alignment.hmm_from,
-                    "aln_hmm_name":          y.alignment.hmm_name.decode(),
-                    "aln_hmm_sequence":      y.alignment.hmm_sequence,
-                    "aln_hmm_to":            y.alignment.hmm_to,
-                    "aln_hmm_length":        y.alignment.hmm_length,
-                    "aln_identity_sequence": y.alignment.identity_sequence,
-                    "aln_target_from":       y.alignment.target_from,
-                    "aln_target_name":       y.alignment.target_name.decode(),
-                    "aln_target_sequence":   y.alignment.target_sequence,
-                    "aln_target_to":         y.alignment.target_to,
-                    'aln_target_length':     y.alignment.target_length
-                    })
-                    
-        if not found:
-    	    r.append({
-		    "hit":None,
-		    "bias":None,
-		    "c_evalue":None,
-		    "correction":None,
-		    "env_from":None,
-		    "env_to":None,
-		    "env_score":None,
-		    "i_evalue":None,
-		    "pvalue":None,
-		    "score":None,
-		    "aln_domain":None,
-		    "aln_hmm_accession":None,
-		    "aln_hmm_from":None,
-		    "aln_hmm_name":None,
-		    "aln_hmm_sequence":None,
-		    "aln_hmm_to":None,
-		    "aln_hmm_length":None,
-		    "aln_identity_sequence":None,
-		    "aln_target_from":None,
-		    "aln_target_name":target_name,
-		    "aln_target_sequence":None,
-		    "aln_target_to":None,
-		    'aln_target_length':None})
-		 
+                        # pyhmmer.plan7.Alignment attributes
+                        "aln_domain":            y.alignment.domain,
+                        "aln_hmm_accession":     y.alignment.hmm_accession.decode(),
+                        "aln_hmm_from":          y.alignment.hmm_from,
+                        "aln_hmm_name":          y.alignment.hmm_name.decode(),
+                        "aln_hmm_sequence":      y.alignment.hmm_sequence,
+                        "aln_hmm_to":            y.alignment.hmm_to,
+                        "aln_hmm_length":        y.alignment.hmm_length,
+                        "aln_identity_sequence": y.alignment.identity_sequence,
+                        "aln_target_from":       y.alignment.target_from,
+                        "aln_target_name":       y.alignment.target_name.decode(),
+                        "aln_target_sequence":   y.alignment.target_sequence,
+                        "aln_target_to":         y.alignment.target_to,
+                        'aln_target_length':     y.alignment.target_length
+                        })
+                        
+            if not found:
+        	    r.append({
+		        "hit":None,
+		        "bias":None,
+		        "c_evalue":None,
+		        "correction":None,
+		        "env_from":None,
+		        "env_to":None,
+		        "env_score":None,
+		        "i_evalue":None,
+		        "pvalue":None,
+		        "score":None,
+		        "aln_domain":None,
+		        "aln_hmm_accession":None,
+		        "aln_hmm_from":None,
+		        "aln_hmm_name":None,
+		        "aln_hmm_sequence":None,
+		        "aln_hmm_to":None,
+		        "aln_hmm_length":None,
+		        "aln_identity_sequence":None,
+		        "aln_target_from":None,
+		        "aln_target_name":target_name,
+		        "aln_target_sequence":None,
+		        "aln_target_to":None,
+		        'aln_target_length':None})
+		     
+        
+        df = pd.DataFrame(r)
+        df['source'] = model
+        results.append(df)
+        
+        if df.empty:
+            print("No results found in HMMER output.")
+            return pd.DataFrame(columns=columns)
     
-    df = pd.DataFrame(r)
+    df = pd.DataFrame(results)
     
-    if df.empty:
-        print("No results found in HMMER output.")
-        return pd.DataFrame(columns=columns)
-
     if columns:
         df = df[columns]
-    
+        
     if rename:
-    	df.rename({'aln_target_name': 'sequence',
-                   'aln_hmm_name': 'model',
-                   'i_evalue': 'evalue',
-                   'env_from': 'estart',
-                   'env_to': 'eend'}, axis=1, inplace=True)
+        df.rename({'aln_target_name': 'sequence', 'aln_hmm_name': 'model', 'i_evalue': 'evalue', 'env_from': 'estart', 'env_to': 'eend'}, axis=1, inplace=True)
 
     return df
 
