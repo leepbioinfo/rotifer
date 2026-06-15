ada74f8c8fa9740b9957aebe612f093c8e4972bc
EdwardNSeven <eduardo_soares@usp.br>
2026-06-15 17:43:08 -0300

    Some tests in epsoares.py

--- lib/rotifer/devel/alpha/epsoares.py
+++ lib/rotifer/devel/alpha/epsoares.py
@@ -23,6 +23,8 @@ import tempfile
 import math
 from joblib import Parallel, delayed
 from multiprocessing import Pool, pool
+from rotifer.genome import utils as rgu
+from rotifer.genome import io as rgio
 
 def get_matrix(df, filter_list, rows, columns, n=10, filter_by='pid'):
         filtered_df = df[df[filter_by].isin(filter_list)]
